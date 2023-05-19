5b59a4eadb2bee52accdb091f140bd9fede9ea6f
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-05-19 09:36:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//greet function
+function greet(firstName) {
+    console.log('Hello There' + firstName)
+}
+
+greet('cj');
+
+//multiply function
+function multiply(number, number) {
+    console.log(4 * 5)
+};
+
+multiply();
+
+//tableOfFive function
+function tableOfFive(number) {
+    return number * number
+}
+tableOfFive(5);
+let number = tableOfFive(5);
+console.log(number);
+
+
